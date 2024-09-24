b797cae963fc8e414041ac37162be826eb2531bb
avca1998 <sfsneed@gmail.com>
2024-09-24 10:01:51 -0700

    initial running code

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,49 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    return decodedToken;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const loggedUser = localStorage.getItem('id_token');
+    if (loggedUser) {
+      return true;
+    } else {
+      return false;
+    }
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    if (decodedToken.exp) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
