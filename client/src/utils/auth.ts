8b98fc045c0af5383099ea70d7d034681c943b82
avca1998 <sfsneed@gmail.com>
2024-09-25 16:34:46 -0700

    refactoring

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,43 +1,32 @@
 import { JwtPayload, jwtDecode } from "jwt-decode";
 class AuthService {
   getProfile() {
-    // TODO: return the decoded token
     const token = this.getToken();
     const decodedToken = jwtDecode<JwtPayload>(token);
     return decodedToken;
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
     const token = this.getToken();
-
     return !!token && !this.isTokenExpired(token);
   }
 
   isTokenExpired(token: string) {
-    // TODO: return a value that indicates if the token is expired
     const decodedToken = jwtDecode<JwtPayload>(token);
-
     return !!decodedToken.exp && decodedToken.exp * 1000 < Date.now();
   }
 
   getToken(): string {
-    // TODO: return the token
     const loggedUser = localStorage.getItem("id_token") || "";
     return loggedUser;
   }
 
   login(idToken: string) {
-    // TODO: set the token to localStorage
-    // TODO: redirect to the home page
-
     localStorage.setItem("id_token", idToken);
     window.location.assign("/");
   }
 
   logout() {
-    // TODO: remove the token from localStorage
-    // TODO: redirect to the login page
     localStorage.removeItem("id_token");
     window.location.assign("/");
   }
