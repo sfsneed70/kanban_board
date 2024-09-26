b7d6af5e95b05073692719423dd9b6eb737ff4f4
avca1998 <sfsneed@gmail.com>
2024-09-26 16:43:25 -0700

    added login props for outlet context

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+
+    rules: {
+
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react/prop-types': 'off',
+    },
+  },
+]
